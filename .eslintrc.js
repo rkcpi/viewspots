1c3490ad6ab2eaf7e3da38f8b4044bd9dd6266be
Sandra Thieme <thieme.sandra@gmail.com>
2022-08-12 12:45:42 +0200

    Add basic setup

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  "rules": {
+    "object-curly-spacing": [2, "always"],
+    "quotes": [2, "single"]
+  }
+};
---
+++ test/index.test.ts
@@ -0,0 +1,8 @@
+import { expect} from 'chai'
+
+describe('A test', () => {
+  it('tests', () => {
+    const lala = 'potato'
+    expect(lala).to.equal('potato')
+  })
+})
